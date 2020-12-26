395b74060004fea752d5916edf455583a21c9752
wannapaly <jasontam225@gmail.com>
2020-12-25 21:16:10 -0500

    ari k

--- src/me.pluginTest.commands/ZombieCommand.java
+++ src/me.pluginTest.commands/ZombieCommand.java
@@ -9,4 +9,15 @@ import me.pluginTest.Main;
 public class ZombieCommand implements CommandExecutor{
     private Main plugin;
 
+    public ZombieCommand(Main plugin){
+        this.plugin=plugin;
+        plugin.getCommand()
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String lable,
+                             String[] args){
+
+    }
+
 }
