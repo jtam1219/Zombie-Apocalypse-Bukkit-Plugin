c482199f8aaabddcfdee28f8c53332dcb571fa27
wannapaly <jasontam225@gmail.com>
2020-12-25 23:15:30 -0500

    ARI IS AWESOME

--- src/me/pluginTest/commands/ZombieCommand.java
+++ src/me/pluginTest/commands/ZombieCommand.java
@@ -1,6 +1,7 @@
 package me.pluginTest.commands;
 
 import org.bukkit.Location;
+import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,6 +9,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import me.pluginTest.Main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.server.PluginEnableEvent;
 
 public class ZombieCommand implements CommandExecutor{
     private Main plugin;
@@ -26,7 +29,12 @@ public class ZombieCommand implements CommandExecutor{
         for (int i=0; i<5; i++) {
             w.spawnEntity(loc, EntityType.ZOMBIE);
         }
+        Server s=p.getServer();
+        for (int i=0; i<5; i++) {
+            s.broadcastMessage("Ari is the G o a t.");
+        }
         return false;
     }
+
 
 }
