f1b09a6bab79703cb0342cecd9574d5443fb36c2
wannapaly <jasontam225@gmail.com>
2020-12-25 22:31:12 -0500

    ari k

--- src/me/pluginTest/Main.java
+++ src/me/pluginTest/Main.java
@@ -1,11 +1,12 @@
 package me.pluginTest;
 
+import me.pluginTest.commands.ZombieCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        new ZombieCommand(this);
     }
 }
--- src/me/pluginTest/commands/ZombieCommand.java
+++ src/me/pluginTest/commands/ZombieCommand.java
@@ -1,9 +1,12 @@
 package me.pluginTest.commands;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import me.pluginTest.Main;
 
 public class ZombieCommand implements CommandExecutor{
@@ -11,13 +14,19 @@ public class ZombieCommand implements CommandExecutor{
 
     public ZombieCommand(Main plugin){
         this.plugin=plugin;
-        plugin.getCommand()
+        plugin.getCommand("test").setExecutor(this);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String lable,
+    public boolean onCommand(CommandSender sender, Command cmd, String label,
                              String[] args){
-
+        Player p=(Player) sender;
+        Location loc=p.getLocation();
+        World w=p.getWorld();
+        for (int i=0; i<5; i++) {
+            w.spawnEntity(loc, EntityType.ZOMBIE);
+        }
+        return false;
     }
 
 }
