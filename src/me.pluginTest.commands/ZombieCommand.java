aa698bfaf47d693251ee11360035a4b17d838ffb
wannapaly <jasontam225@gmail.com>
2020-12-25 20:59:30 -0500

    ari k

---
+++ src/me.pluginTest.commands/ZombieCommand.java
@@ -0,0 +1,12 @@
+package me.pluginTest.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import me.pluginTest.Main;
+
+public class ZombieCommand implements CommandExecutor{
+    private Main plugin;
+
+}
--- src/me.pluginTest/Main.java
+++ src/me.pluginTest/Main.java
@@ -3,5 +3,9 @@ package me.pluginTest;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin{
-  
+
+    @Override
+    public void onEnable(){
+
+    }
 }
