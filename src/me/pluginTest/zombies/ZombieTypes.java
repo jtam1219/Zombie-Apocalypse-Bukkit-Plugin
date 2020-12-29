9b5a0afd36ba16f273c6f1c3574392bfe90405a7
wannapaly <jasontam225@gmail.com>
2020-12-28 21:14:58 -0500

    ARI IS AWESOME

--- src/me/pluginTest/zombies/ZombieTypes.java
+++ src/me/pluginTest/zombies/ZombieTypes.java
@@ -15,7 +15,6 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
