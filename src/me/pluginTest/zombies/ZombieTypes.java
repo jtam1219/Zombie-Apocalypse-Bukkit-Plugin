fe9afec1582c46ec00c6c28c0fa6baf06a96c3e0
wannapaly <jasontam225@gmail.com>
2021-01-23 16:41:01 -0500

    ARI IS AWESOME

--- src/me/pluginTest/zombies/ZombieTypes.java
+++ src/me/pluginTest/zombies/ZombieTypes.java
@@ -118,14 +118,29 @@ public class ZombieTypes implements Listener {
   public void tankHealthChecker(EntityDamageEvent e) {
     if (e.getEntity() instanceof Zombie && e.getEntity().hasMetadata("Tank")) {
       Zombie tank = (Zombie) e.getEntity();
-      // if (!tank.hasPotionEffect(PotionEffectType.ABSORPTION)) {
-      // tank.getServer().broadcastMessage("Health: " + (tank.getHealth() -
-      // e.getDamage()));
-      // } else {
-      // tank.getServer().broadcastMessage("Health: " + tank.getHealth());
-      // }
+       //if (!tank.hasPotionEffect(PotionEffectType.ABSORPTION)) {
+       //tank.getServer().broadcastMessage("Health: " + (tank.getHealth() -
+       //e.getDamage()));
+      //} else {
+       tank.getServer().broadcastMessage("Health: " + tank.getHealth());
+       //}
     }
   }
+
+  @EventHandler
+  public void bulwarkHealthChecker(EntityDamageEvent e) {
+    if (e.getEntity() instanceof Zombie && e.getEntity().hasMetadata("bulwark")) {
+      Zombie bulwark = (Zombie) e.getEntity();
+      //if (!bulwark.hasPotionEffect(PotionEffectType.ABSORPTION)) {
+       // bulwark.getServer().broadcastMessage("Health: " + (bulwark
+      // .getHealth() -
+       //         e.getDamage()));
+      //} else {
+        bulwark.getServer().broadcastMessage("Health: " + bulwark.getHealth());
+      //}
+    }
+  }
+
 
   @EventHandler
   public void tankFallDamage(EntityDamageEvent e) {
@@ -326,8 +341,9 @@ public class ZombieTypes implements Listener {
             zombie.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).setBaseValue(.85);
             zombie.getAttribute(Attribute.GENERIC_FOLLOW_RANGE).setBaseValue(35);
             zombie.getAttribute(Attribute.ZOMBIE_SPAWN_REINFORCEMENTS).setBaseValue(.5);
-            zombie.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION,
+            zombie.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST,
                     1000000, 14));
+            zombie.addPotionEffect(new PotionEffect(PotionEffectType.HARM, 1, 100));
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1000000, 2));
             zombie.getEquipment().setHelmetDropChance(0.15f);
             zombie.getEquipment().setChestplateDropChance(0.05f);
@@ -391,9 +407,11 @@ public class ZombieTypes implements Listener {
             zombie.getEquipment().setBootsDropChance(0.05f);
             zombie.getEquipment().setItemInMainHandDropChance(0.05f);
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1000000, 1));
-            zombie.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION,
+            zombie.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST,
                     1000000, 19));
+            zombie.addPotionEffect(new PotionEffect(PotionEffectType.HARM, 1, 100));
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1000000, 0));
+            zombie.setMetadata("bulwark", new FixedMetadataValue(plugin, "bulwark"));
           }
         }
       }
